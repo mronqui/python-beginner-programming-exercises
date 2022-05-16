73ca19882107c56a81dfacd4544d9692d093e2b2
Mauricio Ronqui <mronqui@gmail.com>
2022-05-16 14:18:43 +0000

    van

--- exercises/16-Random-Colors-Loop/app.py
+++ exercises/16-Random-Colors-Loop/app.py
@@ -1,6 +1,6 @@
 import random
 
-def get_color(color_number=4):
+def get_color(color_number):
     # making sure is a number and not a string
     color_number = int(color_number)
 
@@ -15,9 +15,12 @@ def get_color(color_number=4):
 
 
 def get_allStudentColors():
-    example_color = get_color(1)
+    example_color = get_color(random.randint(0,3))
     students_array = []
     #your loop here
-
+	for i in range(1,11):
+		students_array
+        
+	return i
 
 print(get_allStudentColors())
